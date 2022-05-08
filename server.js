9962e4b2bd0bc5226349ab40d315fdb3eab924ab
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-08 20:53:26 +0700

    filter to specific object

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
+
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+};
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
-})
+
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+
+
+});
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
