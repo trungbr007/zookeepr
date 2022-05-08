8a88ef81447d7ea4d0fc398ae808a4d8d1848b85
Trung Nguyen <trungnguyen@Trungs-MacBook-Pro.local>
2022-05-08 16:42:54 +0700

    Add port to heroku

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express =require ('express');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
-app.listen(3001,() =>{
-  console.log(`API server npw on port 3001!`)
-});
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+})
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
